e7f2bbb4d24c291ca57a5fc54e5d5c698a6c1267
Roman <bromanla.json@gmail.com>
2021-09-23 22:29:27 +0300

    add broadcast function

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,32 @@
-int counter = 0;
-
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print(counter);
-  delay(1000);
+  byte test[] = {
+    map(random(0, 128), 0, 1024, 0, 180),
+    map(random(0, 128), 0, 1024, 0, 180),
+    map(random(0, 128), 0, 1024, 0, 180),
+  };
 
-  counter = counter > 128 ? 0 : counter + 1;
+  broadcast(test, sizeof(test));
+}
+
+// формирование сообщения и передача по COM порту
+void broadcast (byte *req, byte reqSize) {
+  static unsigned long timer = millis();
+
+  if (millis() - timer <= 200)
+    return;
+
+  timer = millis();
+  char res[] = "";
+
+  // sizeof Byte array = lenght array
+  for (byte i = 0; i < reqSize; i++) {
+    itoa(req[i], strchr(res, NULL), DEC);
+    strcat(res, i == reqSize - 1 ? ";" : ",");
+  }
+
+  Serial.print(res);
 }

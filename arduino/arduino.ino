494808cb27b656b907aeec7d84c030cb8fdb22f8
Roman (Work) <bromanla.json@gmail.com>
2021-09-27 15:17:40 +0300

    add analogRead in arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,5 +1,7 @@
 #define COUNT_JOINTS 3
 #define DEGREES_JOINTS 180
+#define BROADCAST_DELAY 100
+#define DEBUG 0
 
 void setup() {
   Serial.begin(9600);
@@ -8,23 +10,32 @@ void setup() {
 void loop() {
   static unsigned long timer = millis();
 
-  if (millis() - timer <= 1000)
-    return false;
+  if (millis() - timer <= BROADCAST_DELAY)
+    return;
 
   timer = millis();
 
   byte data[COUNT_JOINTS];
-  mockAnanlogRead(data);
+
+  if (DEBUG)
+    mockAnanlogRead(data);
+  else
+    ananlogRead(data);
+
   comBroadcast(data);
 }
 
-void mockAnanlogRead (byte *mock) {
-  for(int i = 0; i < COUNT_JOINTS; i++) {
-    mock[i] = random(0, 180);
-  }
+void ananlogRead (byte *data) {
+  for(int i = 0; i < COUNT_JOINTS; i++)
+    data[i] = map(analogRead(14 + i), 0, 1023, 0, 180);
 }
 
-// формирование сообщения и передача по COM порту
+void mockAnanlogRead (byte *mock) {
+  for(int i = 0; i < COUNT_JOINTS; i++)
+    mock[i] = random(0, 180);
+}
+
+// COM PORT DETERMINATOR \r\n
 void comBroadcast (byte *req) {
   char res[] = "";
 

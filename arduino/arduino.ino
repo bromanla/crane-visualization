f1ffdaf1f1e21d4cfbd6c60cb0a49a91db80a279
Roman (Work) <bromanla.json@gmail.com>
2021-09-29 13:31:49 +0300

    add comHealth function fix arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,38 +1,47 @@
-#define COUNT_JOINTS 3
-#define DEGREES_JOINTS 180
-#define BROADCAST_DELAY 100
-#define DEBUG 0
+#define COUNT_JOINTS 3 // количество датчиков
+#define DEGREES_JOINTS 180 // максимальный угол поворота
+#define BROADCAST_DELAY 250 // задержка передачи
+#define DEBUG 0 // 0 - датчики, 1 - случайные данные
 
 void setup() {
   Serial.begin(9600);
+  Serial.setTimeout(128);
 }
 
 void loop() {
   static unsigned long timer = millis();
-
-  if (millis() - timer <= BROADCAST_DELAY)
-    return;
-
-  timer = millis();
-
   byte data[COUNT_JOINTS];
+  comHealth();
 
-  if (DEBUG)
-    mockAnanlogRead(data);
-  else
-    ananlogRead(data);
+  if (millis() - timer >= BROADCAST_DELAY) {
+    timer = millis();
 
-  comBroadcast(data);
+    if (DEBUG)
+      mockAnanlogRead(data);
+    else
+      ananlogRead(data);
+
+    comBroadcast(data);
+  }
 }
 
-void ananlogRead (byte *data) {
+void comHealth () {
+  if (Serial.available()) {
+    int command = Serial.parseInt();
+
+    if (command == 0)
+      Serial.println("ok");
+  }
+}
+
+void ananlogRead (byte *d) {
   for(int i = 0; i < COUNT_JOINTS; i++)
-    data[i] = map(analogRead(14 + i), 0, 1023, 0, 180);
+    d[i] = map(analogRead(14 + i), 0, 1023, 0, DEGREES_JOINTS);
 }
 
 void mockAnanlogRead (byte *mock) {
   for(int i = 0; i < COUNT_JOINTS; i++)
-    mock[i] = random(0, 180);
+    mock[i] = random(0, DEGREES_JOINTS);
 }
 
 // COM PORT DETERMINATOR \r\n
